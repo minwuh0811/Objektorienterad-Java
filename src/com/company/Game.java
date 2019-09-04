51cdf6926ab7d54578492adbc0e2041ac4cef1b1
minwuh081@gmail.com <minwuh081@gmail.com>
2019-09-04 14:20:25 +0200

    Lose extra life when in the dangerous range

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -14,19 +14,30 @@ public class Game {
         System.out.println("Player 1 set a number:");
         String string=sc.nextLine();
         int player1=Integer.parseInt(string);
-        int i=1;
-        while(i<=5) {
-            if (i == 6) {
+        int i=10;
+        while(i>-1) {
+            if (i == -1) {
                 System.out.println("Oops!! You lose the Game!!");
             } else {
                 System.out.println("Player 2 guess a number:");
                 int player2 = Integer.parseInt(sc.nextLine());
-                if (player2 > player1) {
+                if (player1-5 <= player2   && player2 <= player1+5 && player2!=player1) {
+                    System.out.println("Oops, you lose en extra life.");
+                    if (player2>player1) {
+                        System.out.println("The number " + player2 + " is larger than it is supposed to be.\n Continue to guess: ");
+                    } else {
+                        System.out.println("The number " + player2 + " is smaller than it is supposed to be.\n Continue to guess: ");
+                    }
+                    i-=2;
+                    System.out.println("You have " + i + " life left.");
+                } else if (player2 > player1+5) {
                     System.out.println("The number " + player2 + " is larger than it is supposed to be.\n Continue to guess: ");
-                    i++;
-                } else if (player2 < player1) {
+                    i--;
+                    System.out.println("You have " + i + " life left.");
+                } else if (player2 < player1-5) {
                     System.out.println("The number " + player2 + " is smaller than it is supposed to be.\n Continue to guess: ");
-                    i++;
+                    i--;
+                    System.out.println("You have " + i + " life left.");
                 } else if (player2 == player1) {
                     System.out.println(" You win!!");
                     break;
